537378949b5da0c7311c1af2332d09808a31a6fa
shobuj <shobujsa93@gmail.com>
2019-01-15 13:07:10 +0600

    adminity admin template simple mertering and authintication done

---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,24 @@
+
+<script data-cfasync="false" src="{{ asset('admin') }}/{{ asset('admin') }}/{{ asset('admin') }}/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/jquery/js/jquery.min.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/jquery-ui/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/popper.js/js/popper.min.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/bootstrap/js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/jquery-slimscroll/js/jquery.slimscroll.js"></script>
+
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/modernizr/js/modernizr.js"></script>
+
+<script type="text/javascript" src="{{ asset('admin') }}/files/bower_components/chart.js/js/Chart.js"></script>
+
+<script src="{{ asset('admin') }}/files/assets/pages/widget/amchart/amcharts.js"></script>
+<script src="{{ asset('admin') }}/files/assets/pages/widget/amchart/serial.js"></script>
+<script src="{{ asset('admin') }}/files/assets/pages/widget/amchart/light.js"></script>
+<script src="{{ asset('admin') }}/files/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/assets/js/SmoothScroll.js"></script>
+<script src="{{ asset('admin') }}/files/assets/js/pcoded.min.js"></script>
+
+<script src="{{ asset('admin') }}/files/assets/js/vartical-layout.min.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/assets/pages/dashboard/custom-dashboard.js"></script>
+<script type="text/javascript" src="{{ asset('admin') }}/files/assets/js/script.min.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>@yield('page-title') </title>
+
+
+<!--[if lt IE 10]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="description" content="#">
+<meta name="keywords" content="Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
+<meta name="author" content="#">
+
+<link rel="icon" href="https://colorlib.com//polygon/adminty/files/assets/images/favicon.ico" type="image/x-icon">
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('admin') }}/files/bower_components/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('admin') }}/files/assets/icon/feather/css/feather.css">
+
+<link rel="stylesheet" type="text/css" href="{{ asset('admin') }}/files/assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('admin') }}/files/assets/css/jquery.mCustomScrollbar.css">
+
+@yield('head-section')
+
+@yield('style-section')
+
+</head>
+<body>
+
+@include('admin.includes.themeLoader')
+
+<div id="pcoded" class="pcoded">
+<div class="pcoded-overlay-box"></div>
+<div class="pcoded-container navbar-wrapper">
+<nav class="navbar header-navbar pcoded-header">
+<div class="navbar-wrapper">
+
+
+@include('admin.includes.navTop')
+
+
+@include('admin.includes.leftMenu')
+
+<div class="pcoded-content">
+	<div class="pcoded-inner-content">
+		<div class="main-body">
+			<div class="page-wrapper">
+
+@yield('main-content')
+
+			</div>
+			<div id="styleSelector"></div>
+		</div>
+	</div>
+</div>
+
+</div>
+</nav>
+</div>
+</div>
+
+
+
+@include('admin.includes.footer')
+
+@yield('footer-section')
+
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+</body>
+
+</html>
